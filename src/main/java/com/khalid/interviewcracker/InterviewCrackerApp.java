51d24122b1b035aefecaad353b0218c71b481d55
Zia Khalid <zkhalid@DELC02NWGN6G3QD.sea.corp.expecn.com>
2017-03-12 16:06:46 +0530

    Setting up the project

---
+++ src/main/java/com/khalid/interviewcracker/InterviewCrackerApp.java
@@ -0,0 +1,14 @@
+package com.khalid.interviewcracker;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewCrackerApp {
+
+    public static void main(String... args){
+        SpringApplication.run(InterviewCrackerApp.class, args);
+    }
+
+}
